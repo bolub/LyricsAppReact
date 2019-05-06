af5e01d2c915e5a6a23ebafe7719f78ef65273c7
abiola boluwatife <abiol5202@gmail.com>
2019-05-06 23:55:18 +0100

    Finished building lyrics app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    data:'',
+    Artist:'',
+    export:'',
+    spinner: false
+  }
+
+  onSubmitHandler = (event)=>{
+    event.preventDefault();
+    let text = this.refs.text.value;
+    let Artist = this.refs.Artist.value;
+    this.setState({data:text, Artist: Artist, spinner: true})
+  } 
+
+  componentDidUpdate(){
+    axios.get("https://api.lyrics.ovh/v1/" + this.state.Artist + "/" + this.state.data)
+      .then(response => {
+        this.setState({ export: response.data.lyrics, spinner: false });
+      } )
+      .catch(e =>{
+        this.setState({ export: "Please try again", spinner: false });
+      })
+    ;
+  }
+
+  showLyricsHandler = ()=>{
+    const doesShow = this.state.showLyrics;
+    this.setState({showLyrics:!doesShow})
+  }
+
   render() {
+    let showSpinner = null; 
+
+    if(this.state.showSpinner){
+      showSpinner = (
+        <div className="spinner mt-5"></div>
+      )
+    }
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <h1>Lyrics App</h1>
+
+        <form onSubmit={this.onSubmitHandler} className="form">
+          <input type="text" placeholder="Song name" className="one py-3" ref="text" />
+          <input type="text" placeholder="Artist name" className="two py-3" ref="Artist" />
+          <button type="submit" className="three py-3 btn">Search<i className="fal fa-search"></i></button>
+        </form>
+
+        <div className="card mt-5">
+          {showSpinner}
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {this.state.export}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        </div> 
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './Assets/fontAwesomepro/css/all.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
